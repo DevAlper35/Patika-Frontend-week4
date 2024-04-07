e43b7f357a9cf7a7d0b435e9541a70c8bc93062f
DevAlper35 <devcalisma@gmail.com>
2024-04-07 23:08:39 +0300

    Update clock.js

--- odev1/clock.js
+++ odev1/clock.js
@@ -2,6 +2,18 @@ let inputName = prompt("İsim giriniz pilis")
 let myNameDOM = document.querySelector("#myName")
 myNameDOM.innerHTML = inputName
 
-let now = new Date ( ) ;
-let myClockDOM = document.querySelector("#myClock")
-myClockDOM.innerHTML = now
+function nowTime() {
+    var days = ["Pazar", "Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", "Cumartesi"];
+    var now = new Date();
+    var hour = now.getHours();
+    var min = now.getMinutes();
+    var sec = now.getSeconds();
+    var day = now.getDay();
+    
+    var dayName = days[day];
+    
+    var myClockDOM = document.getElementById("myClock");
+    myClockDOM.innerText = hour + ":" + min + ":" + sec + " " + dayName;
+}
+
+setInterval(nowTime, 1000);
